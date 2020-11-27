d8737fcc9f8bf45c38050133e862bfcd1c12c84f
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-27 15:05:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -36,9 +36,7 @@ while($row = mysqli_fetch_array($res))
     <td><div align="center"><?php echo $row['Price'];?></div></td>
     <td><div align="center"><?php echo $row['Discount'];?></div></td>
     <td><div align="center"><?php echo $row['Total'];?></div></td>
-    <td><div align="center">
-    <a href="edit.php?id=<?php echo $row['ID'];?>" class="btn btn-primary">EDIT</a></div>
-    </td>
+    <td><a align="center" href="edit.php?id=<?php echo $row['ID'];?>" class="btn btn-primary">EDIT</a></td>
   </tr>
 <?php
 }
